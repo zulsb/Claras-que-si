cd3901e113b300e896dd2ce8add8225dbd821a5e
zulsb <zulsb2093@gmail.com>
2021-04-30 22:33:20 -0500

    Fix footer

--- src/components/Assets/Styles/Styled.js
+++ src/components/Assets/Styles/Styled.js
@@ -338,7 +338,7 @@ export const FooterSection = styled.footer`
   background: #F2F0F0;
   border-top: 13px solid #FF9900;
   border-bottom: 13px solid #4A1176;
-  padding: 30px;
+  padding: 22px;
   /* position:absolute; */
   bottom: 0;
   left: 0;
