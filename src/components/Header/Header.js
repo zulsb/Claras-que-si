670c91e463e33118cf41752081e944c90d70d86e
zulsb <zulsb2093@gmail.com>
2021-05-08 03:04:09 -0500

    Delete: Link router header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,23 +2,21 @@ import React from 'react';
 import * as styled from "../Assets/Styles/Styled";
 import logo from "../Assets/Images/logo.png";
 import LogOut from "../LogOut/LogOut";
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <React.Fragment>
             <styled.Header>
                     <styled.ContainerHome>
-                        <styled.HeaderContainer>
-                            <Router>
-                                <div><styled.Logo src={logo} alt="Logo"/></div> 
-                                <styled.Nav>
-                                    <li><styled.NavLink><Link to={"/"}><styled.HomeIcon />Inicio</Link></styled.NavLink></li>
-                                    <li><styled.NavLink><Link to={"/notifications"}><styled.NotiIcon />Notificaciones</Link></styled.NavLink></li>
-                                    <li><styled.NavLink><Link to={"/income"}><styled.UserIcon />Perfil</Link></styled.NavLink></li>
-                                    <li><LogOut>Cerrar sesión</LogOut></li>
-                                </styled.Nav>          
-                            </Router>
+                        <styled.HeaderContainer>                            
+                            <div><styled.Logo src={logo} alt="Logo"/></div> 
+                            <styled.Nav>
+                                <li><styled.NavLink><Link to={"/"}><styled.HomeIcon /> Inicio</Link></styled.NavLink></li>
+                                <li><styled.NavLink><Link to={"/notifications"}><styled.NotiIcon /> Notificaciones</Link></styled.NavLink></li>
+                                <li><styled.NavLink><Link to={"/income"}><styled.UserIcon /> Perfil</Link></styled.NavLink></li>
+                                <li><LogOut>Cerrar sesión</LogOut></li>
+                            </styled.Nav>
                         </styled.HeaderContainer>
                     </styled.ContainerHome>
                 </styled.Header>
